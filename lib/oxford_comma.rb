2b871a231dfeb28db7ba0fc5df145c7af8b9e876
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-04 21:48:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-  (array.join(", ")).insert(-2, "and ")
+  array =  []
+  if array.length>2
+    puts (array.join(", ")).insert(-2, "and ")
+  elsif array.length = 2
+    puts array.join(" and ")
+  elsif array.length = 1
+    puts array.join
+  else
+    nil
+  end
 end
