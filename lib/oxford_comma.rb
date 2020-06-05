f5bb62acdc72776c14685b3dfaf6c2ae1c5f35c8
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-05 01:00:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,9 @@
 def oxford_comma(array)
-  array =  []
-  if array.length>2
-    puts (array.join(", ")).insert(-2, "and ")
-  elsif array.length = 2
-    puts array.join(" and ")
-  elsif array.length = 1
-    puts array.join
+  array = []
+  if array.size < 3
+    return array.join(' and ')
   else
-    nil
+    list[-1] = "and" + list[-1]
+    list.join(', ')
   end
 end
