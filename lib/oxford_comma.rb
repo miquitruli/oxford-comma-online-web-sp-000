8149a6a985c5e53bd1dcf5b492d217eb480a56b4
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-05 01:19:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.size < 3
+    return array.join(" and ")
+  elsif array.size > 2
+    array[-1] = "and " + array[-1]
+    return array.join(', ')
+  end
 end
