c0f00f9003c1a450fb0f7d92f8285b574ad1c3f4
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-05 01:12:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.size < 3
     return array.join(" and ")
   elsif array.size > 2
-    list[-1] = "and " + list[-1]
-    return list.join(', ')
+    array[-1] = "and " + array[-1]
+    return array.join(', ')
   end
 end
